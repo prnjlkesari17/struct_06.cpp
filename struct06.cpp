536e9f437ab80f459ce40aa50ab05645f29a11a0
prnjlkesari17 <prnjlkesari1710@gmail.com>
2025-11-18 05:32:50 +0530

    Create struct06.cpp

---
+++ struct06.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std; 
+struct emp; 
+{
+int id; 
+float salaray ;
+char name[20];
+}
+int main()
+{
+struct emp e1; 
+cout<<"enter the employee infor";
+cin>>e1.id;
+cin>>e1.salary;
+cin>>e1.name;
+cout<<"the name of employee"<<e1.name<<endl;
+cout<<"the salary of employee"<<el.salary; 
+cout<<"the id of employee"<<e1.id;
+}
